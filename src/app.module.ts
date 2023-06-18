fff7dc386b033ba27767e9636e0bc6db0095e356
HoleHorse <tamirlankulzhanov@gmail.com>
2023-06-19 00:01:48 +0600

    auth bare, change to local db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://tkulzhan:tkulzhan@cluster.czbqaif.mongodb.net/movietestcase?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot('mongodb://localhost:27017/movietestcase'),
     MoviesModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
